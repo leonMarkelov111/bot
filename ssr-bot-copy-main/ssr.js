c24229650255a0e297990bf2fc22695d57e5aa80
leonMarkelov111 <85875697+leonMarkelov111@users.noreply.github.com>
2022-05-22 19:00:59 +0300

    Add files via upload

--- ssr-bot-copy-main/ssr.js
+++ ssr-bot-copy-main/ssr.js
@@ -9,25 +9,36 @@ const {VK} = require('vk-io');
 const messages = require("./message.json");
 const {checkUserInBlackList} = require("./src/func");
 global.vk = new VK({
-    token: 'a85e421554a847c2f5f639521088b00e081c8672b0604a73142277af05b23e9918016ff520cb0459aaf7e',
+    token: 'bd0bad70d5169d9d298b705ca5e159f1a4d34dc272287969dc8da141892d84d517d06d0ab084e5c5f68ed',
     pollingGroupId: 213308439,
     apiMode: 'parallel'
 });
 
 //====================== MongoDB ===========================================================
-global.mongoose = require('mongoose');
-mongoose.connect(PASTE_LOGIN).then(() => logger.info.info("Подключение к БД - успешно!"))
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Leon_Markelovv:4wrEAGkK06ohFXAg@cluster0.p4yss.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+  
+// установка схемы
+const userScheme = new Schema({
+    name: String,
+    age: Number
 });
-
-
+  
+// подключение
+mongoose.connect("mongodb://localhost:27017/usersdb", { useUnifiedTopology: true, useNewUrlParser: true });
+  
+const User = mongoose.model("User", userScheme);
+const user = new User({
+    name: "Leon_Markelovv",
+    age: 41
+});
+  
+user.save(function(err){
+    mongoose.disconnect();  // отключение от базы данных
+      
+    if(err) return console.log(err);
+    console.log("Сохранен объект", user);
+});
 // Объявляем объект с командами
 
 const cmds = fs
